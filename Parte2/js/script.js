5b148bbec1e998baea907e7bfe00fcb830efe2b8
Christopher Dorado <a01235737@itesm.mx>
2022-07-11 17:29:59 -0500

    examparc1

---
+++ Parte2/js/script.js
@@ -0,0 +1,47 @@
+      
+
+      /*
+        Desarrolla la lógica para convertir los valores del textArea en minusculas: 
+
+        Tip: Divide el problema en partes pequeñas. 
+
+        1) Primero recupera los valores utilizando el DOM y los metodos que vimos en clase. 
+        2) Parte los valores por la coma (,). TIP. Utiliza la función split
+        3) convierte los valores a minuscula con alguna de las funciones de javascript. 
+        4) Escribe en el textArea los nuevos valores
+
+        Realiza todo lo anterior al hacer click en el botón. 
+        */
+
+
+      /*
+        Crea una funcion llamada "calculateDogAge" que: 
+    
+    - reciba como argumento un número representando la edad en años humanos.
+    - realiza el cálculo de la edad en base a la conversión 1 año humano = 7 años perro.  
+    - Deberá escribir en valor convertido. 
+    - Recuerda utilizar preventDefault() como lo vimos en clase para evitar que el formulario se envie. 
+    - Ojo: Cuando se lee un valor de cualquier formulario, el tipo de dato por default es string. 
+           Deberás convertir el string a Entero. 
+    
+    */
+
+
+
+      /*  OBJETOS 
+    
+    A) Crea un constructor de tipo "Pokemon" que reciba 4 parametros
+    
+    1) Nombre
+    2) Especie
+    3) Ataque
+    4) Defensa
+    
+    
+    B) Crea 2 objectos  con diferentes valores. Para los despistados, existen 18 especies de pokeon: 
+       Bicho, Dragón, Eléctrico, Hada, Lucha, Fuego, Volador, Fantasma, Planta, Tierra, Hielo, Normal, Veneno, Psíquico, Roca, Acero y Agua
+       El Nombre y la Especie son Strings, Ataque y Defensa numericos.
+       
+       Registra un evento de "click", para que cuando se de click en el botón, se muestren las propiedades de cada objeto en el input correspondiente. 
+
+     */
